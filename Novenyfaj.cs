a4d0cc93c72cfdd8bcfc6c50f37a5833af52d8bd
Major Bence <majorbence719@gmail.com>
2026-02-19 14:52:19 +0100

    novenyfaj kesz

--- Novenyfaj.cs
+++ Novenyfaj.cs
@@ -8,5 +8,36 @@ namespace UveghazProject
 {
 	internal class Novenyfaj
 	{
+		private string nev;
+		private int vizigeny; //max 100%
+		private int egeszsegAllapot; //0-10, ahol 0 ott halott noveny
+		private int optimalisSuruseg;
+
+		public Novenyfaj(string nev, int vizigeny, int egeszsegAllapot, int optimalisSuruseg)
+		{
+			
+			this.Nev = nev;
+			this.Vizigeny = vizigeny;
+			this.EgeszsegAllapot = egeszsegAllapot;
+			this.OptimalisSuruseg = optimalisSuruseg;
+		}
+
+		public string Azonosito { get => this.nev.Substring(0, 3); }
+		public string Nev { get => nev; set => nev = value; }
+		public int Vizigeny { get => vizigeny; set => vizigeny = value; }
+		public int EgeszsegAllapot { get => egeszsegAllapot; set => egeszsegAllapot = value; }
+		public int OptimalisSuruseg { get => optimalisSuruseg; set => optimalisSuruseg = value; }
+
+
+		public override string ToString()
+		{
+			return $"{this.nev} ({this.Azonosito}) {this.vizigeny} {this.egeszsegAllapot}";
+		}
+	
+	
+	
 	}
 }
+
+
+	
--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
          Kezelo k = new Kezelo("Kovacs Peter", "kpeter", Szerepkor.ADMIN);
-            Kezelo k1 = new Kezelo("Nagy Anna", "nanna", Szerepkor.KERTESZ);
+         Kezelo k1 = new Kezelo("Nagy Anna", "nanna", Szerepkor.KERTESZ);
 
 
 
